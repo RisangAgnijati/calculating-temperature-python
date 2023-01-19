d1ed611c1abbb443de824f05a8f7c8c557177585
unknown <risangagnijati66@gmail.com>
2023-01-19 21:20:08 +0700

    full code

---
+++ Menghitung suhu.py
@@ -0,0 +1,17 @@
+#Celcius
+print("=====PROGRAM KONVERSI TEMPERATUR=====")
+
+celcius = float(input('Masukan suhu dalam bentuk celcius : '))
+print("suhu adalah",celcius, "Celcius")
+
+# Reamur
+reamur = (4/5) * celcius
+print("suhu dalam reamur adalah",reamur, "Reamur")
+
+# fahrenheit
+fahrenheit = ((9/5) * celcius) + 32
+print("suhu dalam fahrenheit adalah",fahrenheit, "Fahrenheit")
+
+# kelvin
+kelvin = celcius + 273
+print("suhu dalam kelvin adalah",kelvin, "Kelvin")
